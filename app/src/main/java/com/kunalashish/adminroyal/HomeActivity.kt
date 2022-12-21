02db6af5f70170b3126d19807bcfead6a194dfb5
kunal <Kunalvirendra2510@outlook.com>
2022-12-21 12:57:03 +0530

    sdcsdhvbsd

--- app/src/main/java/com/kunalashish/adminroyal/HomeActivity.kt
+++ app/src/main/java/com/kunalashish/adminroyal/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.kunalashish.adminroyal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kunalashish.adminroyal.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/kunalashish/adminroyal/RegisterActivity.kt
+++ app/src/main/java/com/kunalashish/adminroyal/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.kunalashish.adminroyal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kunalashish.adminroyal.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+        binding.haveAccount.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
     }
 }
