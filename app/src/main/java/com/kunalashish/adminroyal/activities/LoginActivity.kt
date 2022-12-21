f21cc2b5cddba70690f4a9f35b0d239717d9e039
ashish8976 <113491231+ashish8976@users.noreply.github.com>
2022-12-22 02:45:15 +0530

    validation on registration form

--- app/src/main/java/com/kunalashish/adminroyal/activities/LoginActivity.kt
+++ app/src/main/java/com/kunalashish/adminroyal/activities/LoginActivity.kt
@@ -3,21 +3,125 @@ package com.kunalashish.adminroyal.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import androidx.appcompat.app.AlertDialog
 import com.kunalashish.adminroyal.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+class LoginActivity : AppCompatActivity() {
+        private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        emailFocusListner()
+        PasswordFocusListner()
+
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+            submitForm()
+
         }
         binding.haventAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+
+
+
     }
+
+    private fun submitForm() {
+        val validEmail = binding.emailContainor.helperText == null
+        val validPassword = binding.passwordContainor.helperText == null
+
+        if (validEmail && validPassword)
+        {
+            reserForm()
+        }
+        else
+        {
+            invalidForm()
+        }
+    }
+
+    private fun invalidForm() {
+        var message = ""
+        if (binding.emailContainor.helperText == null) {
+            message += "\n \n Email : " + binding.emailContainor.helperText
+        }
+        if (binding.passwordContainor.helperText == null){
+            message += "\n \n Password" + binding.passwordContainor.helperText
+        }
+
+        AlertDialog.Builder(
+            this
+        ).setTitle("Please Full fill Details")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_->
+                // do nothing
+            }
+    }
+
+    private fun reserForm() {
+        startActivity(Intent(this, HomeActivity::class.java))
+    }
+
+    private fun emailFocusListner()
+    {
+        binding.edtEmailAddress.setOnFocusChangeListener { _, focused ->
+
+            if(!focused){
+                binding.emailContainor.helperText = validEmail()
+            }
+        }
+    }
+
+    private fun validEmail() : String?{
+        val emailText = binding.edtEmailAddress.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+            return "Invalid Email Address"
+        }
+        return null
+    }
+
+    private fun PasswordFocusListner()
+    {
+        binding.edtPassword.setOnFocusChangeListener { _, focused ->
+            if (!focused)
+            {
+                binding.passwordContainor.helperText = validPassword()
+            }
+
+        }
+    }
+
+    private fun validPassword(): String? {
+        val password = binding.edtPassword.text.toString()
+        if (password.length < 8)
+        {
+            return "Minimum 8 Character Password"
+        }
+        if (password.length > 16)
+        {
+            return "Maximum 16 Character Password"
+        }
+        if (password.matches(".*/[A-Z].*".toRegex()))
+        {
+            return "Must Contain 1 UpperCase Letter"
+        }
+        if (password.matches(".*/[a-z].*".toRegex()))
+        {
+            return "Must Contain 1 Lowercase Letter"
+        }
+        if (password.matches(".*/[@#$%^&+-].*".toRegex()))
+        {
+            return "Must Contain 1 Special Character"
+        }
+
+        return null
+    }
+
+
 }
--- app/src/main/java/com/kunalashish/adminroyal/activities/RegisterActivity.kt
+++ app/src/main/java/com/kunalashish/adminroyal/activities/RegisterActivity.kt
@@ -3,22 +3,124 @@ package com.kunalashish.adminroyal.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import androidx.appcompat.app.AlertDialog
 import com.kunalashish.adminroyal.databinding.ActivityRegisterBinding
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        emailFocusListner()
+        PasswordFocusListner()
+
         binding.btnRegister.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+
+            submitForm()
         }
         binding.haveAccount.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+
+    }
+
+    private fun submitForm() {
+        val validEmail = binding.EmailContainer.helperText == null
+        val validPassword = binding.passwordContainer.helperText == null
+
+        if (validEmail && validPassword)
+        {
+            reserForm()
+        }
+        else
+        {
+            invalidForm()
+        }
+     }
+
+    private fun reserForm() {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    private fun invalidForm() {
+        var message = ""
+        if (binding.EmailContainer.helperText == null) {
+            message += "\n \n Email : " + binding.EmailContainer.helperText
+        }
+        if (binding.passwordContainer.helperText == null){
+            message += "\n \n Password" + binding.passwordContainer.helperText
+        }
+
+        AlertDialog.Builder(
+            this
+        ).setTitle("Please Full fill Details")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_->
+                // do nothing
+            }
+
+    }
+
+    private fun emailFocusListner()
+    {
+        binding.EdtEmail.setOnFocusChangeListener { _, focused ->
+
+            if(!focused){
+                binding.EmailContainer.helperText = validEmail()
+            }
+        }
+    }
+
+    private fun validEmail() : String?{
+            val emailText = binding.EdtEmail.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+            return "Invalid Email Address"
+        }
+        return null
+    }
+    
+    
+    private fun PasswordFocusListner()
+    {
+        binding.edtPassword.setOnFocusChangeListener { _, focused ->
+            if (!focused)
+            {
+                binding.passwordContainer.helperText = validPassword()
+            }
+
+        }
+    }
+
+    private fun validPassword(): String? {
+        val password = binding.edtPassword.text.toString()
+        if (password.length < 8)
+        {
+            return "Minimum 8 Character Password"
+        }
+        if (password.length > 16)
+        {
+            return "Maximum 16 Character Password"
+        }
+        if (password.matches(".*/[A-Z].*".toRegex()))
+        {
+            return "Must Contain 1 UpperCase Letter"
+        }
+        if (password.matches(".*/[a-z].*".toRegex()))
+        {
+            return "Must Contain 1 Lowercase Letter"
+        }
+        if (password.matches(".*/[@#$%^&+-].*".toRegex()))
+        {
+            return "Must Contain 1 Special Character"
+        }
+
+        return null
     }
 }
